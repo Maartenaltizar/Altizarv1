497e8a4e68b96420bb8742a7aef8227c326d0212
Maarten de Ruyter <maarten.altizar@gmail.com>
2011-10-20 21:35:07 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altizarv1::Application.config.secret_token = '917867677b63dfd3e6c09b85cd03c93ed9256e94606cf2f7ba6fbc2fe0f6e6fa87f075af241bd4f188eacb267966e0aa5ece65fbc53e8f4ff3edd8658cc24bc5'
